732e29742b5978d1db2d6fcb2a857a6ec4486d96
bwong07 <47698084+bwong07@users.noreply.github.com>
2019-02-16 10:21:46 -0800

    Add files via upload

---
+++ Load_Cell_Calibration/Load_Cell_Calibration.ino
@@ -0,0 +1,97 @@
+
+#include "HX711.h"
+#include <LiquidCrystal.h>;
+
+#define DOUT  6
+#define CLK  7
+LiquidCrystal lcd(9, 8, 5, 4, 3, 2);
+
+HX711 scale(DOUT, CLK);
+
+float calibration_factor = 7530;                                       //=100g
+float output;
+int readIndex;
+float total=0;
+float average=0;
+float average_last=0;
+const int cycles=20;
+float readings[cycles];
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + or a to increase calibration factor");
+  Serial.println("Press - or z to decrease calibration factor");
+
+  scale.set_scale();
+  scale.tare();  //Reset the scale to 0
+
+  long zero_factor = scale.read_average();                             //Get a baseline reading
+  Serial.print("Zero factor: ");                                       //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
+  lcd.begin(16, 2);
+}
+
+void loop() {
+
+  scale.set_scale(calibration_factor);                                 //Adjust to this calibration factor
+
+  Serial.print("Reading: ");
+  output=scale.get_units(), 2;
+  Serial.print(output);
+  lcd.setCursor(0, 0);
+  lcd.print(calibration_factor);
+  lcd.setCursor(0, 1);
+  lcd.print(output);
+  
+
+
+//Smoothing the results
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the scale
+  readings[readIndex] = scale.get_units(), 2;
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= cycles) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+  // calculate the average:
+  average = total / cycles;
+
+  average=scale.get_units(), 2;
+
+
+//Zero drift compensation
+  if((average_last>average+0.03 || average_last<average-0.03)){
+      //Minimum Load defines Zero-Band
+      if (average<0.06){average=0;}
+      Serial.print("\tFilter: ");
+      Serial.print(average);
+      average_last=average;
+  }
+  else{
+    Serial.print("\tFilter: ");
+    Serial.print(average_last);
+  }
+  Serial.print(" g");                                               //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
+
+  if(Serial.available())
+  {
+    char temp = Serial.read();
+    if(temp == '+' || temp == 'a')
+      calibration_factor += 10;
+    else if(temp == '-' || temp == 'z')
+      calibration_factor -= 10;
+  }
+}
---
+++ Load_Cell_Measurement/Load_Cell_Measurement.ino
@@ -0,0 +1,96 @@
+// Brenden Wong
+// Chavkin Lab
+// SURP 2018
+
+//This sketch allows Arduino Uno to record the mass of a gel cup and write the resulting mass along with a time stamp to an SD card
+
+# include <Time.h>                                 // include time library
+# include <TimeLib.h>
+# include "HX711.h"                                // include load cell library
+# include <SD.h>                                   // include capability to write to SD card
+# include <SPI.h>
+# include <LiquidCrystal.h>
+# define DOUT 6                                    // define pins for load cell
+# define CLK 7
+
+LiquidCrystal lcd(9, 8, 5, 4, 3, 2);
+
+HX711 scale(DOUT, CLK);                            ////////////////////// 
+float calibration_factor = 7530; //<---------------///Adjust as needed///
+char filename[] = "LOADCELL.CSV";                  //////////////////////
+char filenameTime[] = "TIME.CSV";
+int chipSelect = 10;
+File file;
+File fileTime;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(chipSelect, OUTPUT);                        // chip select pin must be set to OUTPUT mode
+  if (!SD.begin(chipSelect)) {                        // Initialize SD card
+    Serial.println("Could not initialize SD card.");  // if return value is false, something went wrong.
+  } 
+  if (SD.exists(filename)) {                          // if file exists, file will be deleted
+    Serial.println("File exists.");
+    if (SD.remove(filename) == true) {
+      Serial.println("Successfully removed file.");
+    } else {
+      Serial.println("Could not removed file.");
+    }
+  }
+  if (SD.exists(filenameTime)) {
+    Serial.println("Time exists.");
+    if (SD.remove(filenameTime) == true) {
+      Serial.println("Successfully removed time file.");
+    } else {
+      Serial.println("Could not remove time.");
+    }
+  }
+  scale.set_scale();
+  scale.tare();                                    //Reset the scale to 0
+  long zero_factor = scale.read_average();         //Get a baseline reading
+  lcd.begin(16,2);
+  lcd.print("Mass is:");
+}
+
+void loop(){
+  scale.set_scale(calibration_factor);             //Adjust to this calibration factor
+  file = SD.open(filename, FILE_WRITE);
+  if (file) {
+    file.println(scale.get_units(), 2);            // write number to file
+    file.close(); // close file
+    Serial.print("Wrote number: ");                // debug output: show written number in serial monitor
+    Serial.println(scale.get_units(), 2);
+  } else {
+    Serial.println("Could not open file (writing).");
+  }
+  fileTime = SD.open(filenameTime, FILE_WRITE);
+  if (fileTime) {
+    fileTime.print(hour());
+    fileTime.print(":");
+    fileTime.print(minute());
+    fileTime.print(":");
+    fileTime.println(second());
+    fileTime.close();
+    Serial.print("Wrote time: ");
+    Serial.print(hour());
+    Serial.print(":");
+    Serial.print(minute());
+    Serial.print(":");
+    Serial.println(second());
+  } else {
+    Serial.println("Could not write time");
+  }
+  Serial.print("Scale reading: ");
+  Serial.println(scale.get_units(), 2); 
+  lcd.setCursor(0, 1);
+  lcd.print(scale.get_units(), 2);
+  delay(2000);                                   // Delay 2 seconds
+}
+
+
+
+
+
+
+
+
